9588a56e98422dd7e94198f9ae149967e0ac2669
Alexandre C. Gerona <alec@alecgerona@gmail.com>
2015-11-30 04:14:01 +0800

    Remove unnecessary footer-user files.

--- application/views/pages/adminhome.php
+++ application/views/pages/adminhome.php
@@ -522,25 +522,3 @@
           </div>
         </div>
     <!--End Modal -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-2.1.4.min.js"); ?>"></script>
-   <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
-   <script type="text/javascript" src="<?php echo base_url("assets/js/editabletable.js"); ?>"></script>
-
-    <!-- Menu Toggle Script -->
-    <script>
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
-    </script>
-
-</body>
-
-</html>
