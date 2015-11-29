bba4c162deef1035ba957aa182eb397f08ce8f62
Alexandre C. Gerona <alec@alecgerona@gmail.com>
2015-11-30 01:20:10 +0800

    Add new modal for confirmation of purchase

--- application/views/pages/adminhome.php
+++ application/views/pages/adminhome.php
@@ -567,6 +567,29 @@
         </div>
     <!--End Modal -->
 
+    <!-- Modal -->
+        <div class="modal fade" id="purchaseModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Confirm Purchase</h4>
+              </div>
+              <div class="modal-body">
+
+              <p>Are you sure you want to purchase?</p>
+
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                
+                <button type="button" class="btn btn-primary" data-dismiss="modal" onclick="insert()">Confirm</button>
+              </div>
+            </div>
+          </div>
+        </div>
+    <!--End Modal -->
+
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
 
