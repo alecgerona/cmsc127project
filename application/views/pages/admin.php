5383dc01fdf4aa55bd2c7bf1b3c6a9322d481e69
Alexandre C. Gerona <alec@alecgerona@gmail.com>
2015-12-04 00:29:39 +0800

    Move edit and delete buttons above ui line.

--- application/views/pages/admin.php
+++ application/views/pages/admin.php
@@ -12,9 +12,9 @@
                 <h4 class="media-heading"><?php echo $row['name'];?><small> Admin</small></h4>
                 <h5>@<?php echo $row['username'];?></h5>
                 <h5>Password: <?php echo $row['password'];?></h5>
-                <hr style="margin:8px auto">
-                <a data-toggle="modal" data-target="#myModal" class="btn-sm btn-warning" onclick="editselection('<?php echo $row['name'];?>', '<?php echo $row['username'];?>')">Edit</a>
+                <a data-toggle="modal" data-target="#myModal" class="btn-sm btn-warning" onclick="editselection('<?php echo $row['name'];?>', '<?php echo $row['username'];?>', '<?php echo $row['password'];?>'')">Edit</a>
                 <a href="#" class="btn-sm btn-danger">Delete</a>
+                <hr style="margin:8px auto">
             </div>
         </div>
 
