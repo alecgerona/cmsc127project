8460844dae299a0c4cdd3c523b52f00d4d6cdcea
Alexandre C. Gerona <alec@alecgerona@gmail.com>
2015-12-04 01:07:57 +0800

    Cut and paste.

--- assets/js/adminselection.js
+++ assets/js/adminselection.js
@@ -2,5 +2,5 @@ function editselection(adminname, adminusername, adminpassword, adminid){
 	document.getElementById("inputadminname").value = adminname;
 	document.getElementById("inputadminusername").value = adminusername;
 	document.getElementById("inputadminpassword").value = adminpassword;
-	document.getElementById("inputadminadminid").value = adminid;		
+	document.getElementById("inputadminadminid").value = adminid;
 }
