2315b7710328a8bdd8e44eb26b07649da2d329ce
Alexandre C. Gerona <alec@alecgerona@gmail.com>
2015-12-03 17:58:30 +0800

    Add dynamic user list on admin page.

--- application/views/pages/admin.php
+++ application/views/pages/admin.php
@@ -19,3 +19,22 @@
 <?php } ?>
 <?php endforeach; ?>
 </div>
+
+<div class ="row">
+<?php foreach ($query2 as $row): { ?>
+       <div class="col-lg-5">
+        <div class="media">
+            <a class="pull-left" href="#">
+                <img class="media-object dp img-circle" src="http://placehold.it/700x400" style="width: 100px;height:100px;">
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading"><?php echo $row['fname'] . " " . $row['mname'] . " " . $row['lname'];?><small> User</small></h4>
+                <hr style="margin:8px auto">
+            </div>
+        </div>
+
+    </div>        
+
+<?php } ?>
+<?php endforeach; ?>
+</div>
