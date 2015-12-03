161f64fdf64a64585a79b0089bab882983c09e8e
Alexandre C. Gerona <alec@alecgerona@gmail.com>
2015-12-04 01:06:54 +0800

    Add sql query to Admin_model to edit admins.

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -9,8 +9,11 @@ class Admin_model extends CI_Model{
 		$name = $this->input->post('inputadminname');
 		$username = $this->input->post('inputadminusername');
 		$password = $this->input->post('inputadminpassword');
-		$id = $this->input-post('inputadminid');
-		
+		$id = $this->input->post('inputadminadminid');
+
+		$sql = "UPDATE admin SET name='$name', username='$username', password='$password' where adminid=$id";
+		$query = $this->db->query($sql);
+
 
 
 	}
