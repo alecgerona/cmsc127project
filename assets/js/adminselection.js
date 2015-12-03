d0255e7ec24e2cec369909e5a7ed88b9cc895ebc
Alexandre C. Gerona <alec@alecgerona@gmail.com>
2015-12-04 04:53:48 +0800

    Add function in JS file for exporting data from table into a JSON string.

--- assets/js/adminselection.js
+++ assets/js/adminselection.js
@@ -1,3 +1,6 @@
+var $BTN = $('#export-btn');
+var $TABLE = $('#ordertable');
+
 function editselection(adminname, adminusername, adminpassword, adminid){
 	document.getElementById("inputadminname").value = adminname;
 	document.getElementById("inputadminusername").value = adminusername;
@@ -31,3 +34,36 @@ function changeyear(){
 	var year = document.getElementById("yearselect").value; //Get the value from the year select
 	document.getElementById("yearchange").value = year; //Change the input value to the selected value
 }
+// A few jQuery helpers for exporting only
+jQuery.fn.pop = [].pop;
+jQuery.fn.shift = [].shift;
+
+$BTN.click(function () {
+
+  var $rows = $TABLE.find('tr:not(:hidden)');
+  var headers = [];
+  var data = [];
+  
+  // Get the headers (add special header logic here)
+  $($rows.shift()).find('th:not(:empty)').each(function () {
+    headers.push($(this).text().replace(/ /g,''));
+  });
+
+  
+  // Turn all existing rows into a loopable array
+  $rows.each(function () {
+    var $td = $(this).find('td');
+    var h = {};
+    
+    // Use the headers from earlier to name our hash keys
+    headers.forEach(function (header, i) {
+      h[header] = $td.eq(i).text();   
+    });
+    
+    data.push(h);
+  });
+  
+  // Output the result
+  window.alert(JSON.stringify(data));
+  document.getElementById("orderlist").value = JSON.stringify(data);
+});
