58e267e48af0ac7f588849f095ca59b503cfa163
Alexandre C. Gerona <alec@alecgerona@gmail.com>
2015-12-03 18:25:31 +0800

    Delete onclick function of modal in admin page.

--- application/views/pages/admin.php
+++ application/views/pages/admin.php
@@ -61,7 +61,7 @@
               <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                 
-                <button type="button" class="btn btn-primary" data-dismiss="modal" onclick="insert()">Confirm</button>
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Confirm</button>
               </div>
             </div>
           </div>
