09d3cebf3c600d791def074ff84027541a63c3f6
Alexandre C. Gerona <alec@alecgerona@gmail.com>
2015-12-03 17:26:51 +0800

    Add User main page with respective header and footer.

---
+++ application/views/pages/admin.php
@@ -0,0 +1,17 @@
+<br>
+<br>
+<div class="row">
+    <div class="col-lg-5">
+        <div class="media">
+            <a class="pull-left" href="#">
+                <img class="media-object dp img-circle" src="https://s3.amazonaws.com/uifaces/faces/twitter/mlane/128.jpg" style="width: 100px;height:100px;">
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">Alexandre C. Gerona<small> Admin</small></h4>
+                <hr style="margin:8px auto">
+
+            </div>
+        </div>
+
+    </div>
+</div>
--- application/views/templates/header-admin.php
+++ application/views/templates/header-admin.php
@@ -1,27 +1,68 @@
-<html>
-        <head>
-                <title>The Burger House</title>
-                <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
-        </head>
-        <body>
+<!DOCTYPE html>
+<html lang="en">
 
-                
-                <nav class="navbar navbar-inverse navbar-fixed-top">
-			      <div class="container">
-			        <div class="navbar-header">
-			          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-			            <span class="sr-only">Toggle navigation</span>
-			            <span class="icon-bar"></span>
-			            <span class="icon-bar"></span>
-			            <span class="icon-bar"></span>
-			          </button>
-			          <a class="navbar-brand" href="#">The Burger House</a>
-			        </div>
-			        <div id="navbar" class="navbar-collapse collapse">
-			          
-			        </div><!--/.navbar-collapse -->
-			      </div>
-			    </nav>
+<head>
 
-                <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-2.1.4.min.js"); ?>"></script>
-				<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>The Burger House - Admin</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
+
+    <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-2.1.4.min.js"); ?>"></script>
+
+    <!-- DataTables CSS -->
+    <link rel="stylesheet" href="<?php echo base_url("DataTables-1.10.9/media/css/jquery.dataTables.css"); ?>" />
+
+    <!-- DataTables -->
+    <script type="text/javascript" src="<?php echo base_url("DataTables-1.10.9/media/js/jquery.dataTables.js"); ?>"></script>
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+ <nav class="navbar navbar-inverse navbar-fixed-top">
+                  <div class="container">
+                    <div class="navbar-header">
+                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                      </button>
+                      <a class="navbar-brand" href="#">The Burger House</a>
+                    </div>
+                    <div id="navbar" class="navbar-collapse collapse">
+                      <ul class="nav navbar-nav">
+
+                    <li>
+                        <a href="user">User Accounts</a>
+                    </li>
+                    <li>
+                        <a href="viewOrderHistory">Order History</a>
+                    </li>
+                    <li>
+                        <a href="inventorypage">Inventory Record</a>
+                    </li>
+                    <li>
+                        <a href="view">Logout</a>
+                    </li>
+                    
+                </ul>
+                    </div><!--/.navbar-collapse -->
+                  </div>
+                </nav>
+        <br>
